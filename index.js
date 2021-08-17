b222766314acf2f7c54a91093fe47c540efa9036
Tobi Ajibade <tobiajibade2017@yandex.com>
2021-08-17 13:47:10 +0100

    folder structure acheived

---
+++ index.js
@@ -0,0 +1,27 @@
+// if(NODE_ENV !== "production") {
+    require("dotenv").config();
+// };
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("hbs", exphbs({defaultLayout: "main", extname:"hbs"}));
+app.set("view engine", "hbs");
+
+app.get('/', (req,res) => {
+    res.render("index");
+})
+
+
+let PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(
+        `Server running on ${PORT}`
+    );
+});
