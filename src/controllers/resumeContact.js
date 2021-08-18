193c3f7110f8737c1809cd4ab65e7ecbf5a480d6
Tobi Ajibade <tobiajibade2017@yandex.com>
2021-08-18 20:44:56 +0100

    Created a notification tunnel for both me and the sender

--- src/controllers/resumeContact.js
+++ src/controllers/resumeContact.js
@@ -1,9 +1,21 @@
+const Contact = require("../datasources/resumeClass");
+
 exports.processForm = async (req,res) => {
     const { fullname, email, phone, msg } = req.body;
 
+    try {
+        let contact = new Contact();
+        
+        // get save data to database ,
     
-    // get save data to database ,
-    // then message your mail for new message alert
-    // also a mail to the sender so i can process it
-
+        if(phone !== undefined) {
+            contact.saveMessageWithPhone(fullname,email,phone,msg);
+        } else {
+            contact.saveMessageWithoutPhone(fullname,email,msg);
+        }
+    
+        
+    } catch (error) {
+        console.log(`Failed to process form`, error);
+    }
 };
--- src/datasources/resumeClass.js
+++ src/datasources/resumeClass.js
@@ -1,21 +1,86 @@
 const db = require("../../configs/db");
+const nodemailer = require("nodemailer");
+
+// create mail transport
+
+let mailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: `${process.env.MAIL}`,
+        pass: `${process.env.MAIL_PASS}`
+    }
+});
 
 class Contact {
     constructor() {}
 
     async saveMessageWithPhone(fullname,email,phone,msg) {
-        db.query(`INSERT INTO resume_contacts(full_name, email, phone_no, message) VALUES($1,$2,$3,$4)`,[fullname,email,phone,msg]);
+       let contact = await  db.query(`INSERT INTO resume_contacts(full_name, email, phone_no, message) VALUES($1,$2,$3,$4) RETURNING full_name, email, message `,[fullname,email,phone,msg]);
+
+    //    alert me for new message
+       this.alertMe(contact.rows[0].full_name, contact.rows[0].message);
+
+    //    alert sender for my response
+    //    get senders firstname
+    let firstname = contact.rows[0].full_name.split(" ")[0];
+       this.alertSender(firstname, contact.rows[0].email);
     };
 
     async saveMessageWithoutPhone(fullname,email,msg) {
-        db.query(`INSERT INTO resume_contacts(full_name, email, message) VALUES($1,$2,$3)`,[fullname,email,msg]);
+       let contact = await  db.query(`INSERT INTO resume_contacts(full_name, email, message) VALUES($1,$2,$3) RETURNING full_name, email, message`,[fullname,email,msg]);
+
+       //    alert me for new message
+       this.alertMe(contact.rows[0].full_name, contact.rows[0].message);
+
+        //    alert sender for my response
+        //    get senders firstname
+        let firstname = contact.rows[0].full_name.split(" ")[0];
+        this.alertSender(firstname, contact.rows[0].email);
+
     };
 
-    async alertMe(email) {
 
-    }
+    async alertMe(senderName, message) {
 
-    async alertSender(email) {
+        let mailOptions = {
+            from: `Tobi Ajibade Resume <noreply@bigg-resume.com>`,
+            to: `herityjohnny14@gmail.com`,
+            subject: `New Message from Resume`,
+            html: `<p>Hey! Tobi, you have a new message from ${senderName} on your resume page.</p>
+                   <p>Message Contents: ${message}</p>
+            `
+        };
+
+        mailTransporter.sendMail(mailOptions, (err,res) => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(`Mail sent to me`, res.response);
+            }
+        });
+
+    };
+
+    async alertSender(senderFirstName, email) {
         
+        let mailOptions = {
+            from: `Tobi Ajibade Resume <noreply@bigg-resume.com>`,
+            to: `${email}`,
+            subject: `Thank You For Contacting Me`,
+            html: `
+                <p>Hi ${senderFirstName}! Thanks for contacting me. I have recieved you message
+                . I will reply you shortly.</p>
+            `
+        };
+        
+        mailTransporter.sendMail(mailOptions, (err,res) => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(`Mail sent `, res.response);
+            }
+        });
     }
 };
+
+module.exports = Contact;
