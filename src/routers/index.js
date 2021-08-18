ae2252c1dd64551599db9b5bbe8967d8047d7ba4
Tobi Ajibade <tobiajibade2017@yandex.com>
2021-08-18 16:27:02 +0100

    Added postgresql database and Contact class controller

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { indexController } = require("../controllers");
+const { processForm } = require("../controllers/resumeContact");
 
 router.get('/', indexController);
+router.post('/submit', processForm);
 
 module.exports = router;
